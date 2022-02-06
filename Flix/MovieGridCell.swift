61d4d7f63bd61b87db6c9469ecc955ca7c51f643
Edward Cluster <ecluster@csumb.edu>
2022-02-05 17:27:43 -0800

    Added unit 2 assignment to repo

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Edward Cluster on 2/5/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
